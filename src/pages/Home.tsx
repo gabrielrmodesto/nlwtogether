057a84d3d82c06e29816ce5193cb110da0d8c6eb
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-07-15 18:28:02 -0300

    Routes between screens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,15 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { useHistory } from 'react-router-dom';
 
 export function Home() {
+	const history = useHistory();
+
+	function navigateToNewRoom(){
+		history.push('/rooms/new');
+	}
+
 	return (
 		<div id="page-auth">
 			<aside>
@@ -16,7 +23,7 @@ export function Home() {
 			<main>
 				<div className="main-content">
 					<img src={logoImg} alt="LetmeAsk" />
-					<button className="createRoom"><img src={googleIconImg} alt="logo do Google" />
+					<button onClick={navigateToNewRoom} className="createRoom"><img src={googleIconImg} alt="logo do Google" />
 					Crie sua sala com o Google
 					</button>
 					<div className="separator">ou entre em uma sala</div>
